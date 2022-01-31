bc22c692a4f466455ab9fa9163faf68cdd14de38
Caio Olivieri <novnew@gmail.com>
2022-01-31 16:39:12 -0300

    An app using react-native

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    Text,
+    TouchableOpacityProps,
+    StyleSheet
+} from 'react-native';
+
+interface SkillCardProps extends TouchableOpacityProps {
+    skill: string
+} 
+
+export function SkillCard({ skill, ...rest } : SkillCardProps) {
+    return(
+        <TouchableOpacity 
+            style={styles.buttonSkill}
+            { ...rest }
+        >
+            <Text style={styles.textSkill}>
+                {skill}            
+            </Text>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    buttonSkill:{
+        backgroundColor: '#1F1e25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 7
+    },
+
+    textSkill: {
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+});
